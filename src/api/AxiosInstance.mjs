87b1ce7a143ededad0e5f002b172d16d9d43fe99
Shageldi-byte <sh.alyyew2019@gmail.com>
2023-02-17 23:46:56 +0500

    server upload

--- src/api/AxiosInstance.mjs
+++ src/api/AxiosInstance.mjs
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-export const BASE_URL = "http://95.85.119.162:5010/api/";
-export const IMAGE_BASE_URL = "http://95.85.119.162:5010/";
+export const BASE_URL = "http://api.elishi.art/api/";
+export const IMAGE_BASE_URL = "http://api.elishi.art/";
 
 const AxiosInstance = axios.create({
     baseURL: BASE_URL,
     timeout: 100000,
     headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("my_token")}`,
     },
 });
 
